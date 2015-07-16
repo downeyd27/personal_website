d7c4a12e8123cc02f3a0f37b77e44a9e869837f3
Dillon Downey <downeydillon@gmail.com>
2015-07-16 12:10:15 -0700

    Display first square on canvas

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
     document.body.style.backgroundColor = this.value;
   }, false); //closes colorInputField event listener
 
+  // canvas javaScript
+  (function initiateCanvas() {
+    var canvas, ctx;
+
+    canvas = document.querySelector("#myCanvas");
+    ctx = canvas.getContext('2d');
+
+    drawPerson();
+
+    function drawPerson() {
+      ctx.fillStyle='#FF0000';
+      ctx.fillRect(10,10,80,100);
+    } // closes drawPerson function
+  }());// closes canvas javaScript
+  // canvas javaScript
 }); // closes document
