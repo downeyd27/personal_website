88db460cce4d86c5b6ae3c611433aa1b0ecb11ed
Afton Downey <downeydillon@gmail.com>
2016-04-21 12:51:43 -0700

    Add meta tag gem and set up defauly meta attributes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,26 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :prepare_meta_tags, if: "request.get?"
+
+  def prepare_meta_tags(options={})
+    site_name   = "Dillon Downey's website"
+    title       = ["Dillon Downey's", action_name].join(" ")
+    description = "This website is a portfolio for showcasing my web development skills and personal endevours."
+    image       = options[:image] || "../assets/images/Dillon-900x600.jpg"
+    # current_url = request.url
+
+    defaults = {
+      site:        site_name,
+      title:       title,
+      image:       image,
+      description: description,
+      keywords:    %w[web software development app junior developer front-end back-end Full-stack ],
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
+  end
 end
