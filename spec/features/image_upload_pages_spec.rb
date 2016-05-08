9b78e11313a9ec5655ea365a945b5964f9d28748
Afton Downey <downeydillon@gmail.com>
2016-05-08 16:04:30 -0700

    Set up crud links and some positioning.

--- spec/features/image_upload_pages_spec.rb
+++ spec/features/image_upload_pages_spec.rb
@@ -1,12 +1,19 @@
+require 'rails_helper'
+
 describe 'upload an image' do
+  before :each do
+    @project = FactoryGirl.create(:project)
+  end
 
   it 'uploads a selected image' do
-    pending raise
-    # visit root_path
-    # click_link 'New Image'
-    # fill_in 'Name', with: 'Mt Hood at Sunrise'
-    # page.attach_file("MtHood", 'spec/fixtures/images/mthoodsunset.jpg')
-    # click_button 'Upload'
-    # expect(page).to have_content 'Mt Hood at Sunrise'
+    visit new_project_path
+    fill_in 'Name', with: "Portfolio picture"
+    fill_in 'Technology:', with: @project.technology
+    fill_in 'Description:', with: @project.description
+    fill_in 'Purpose:', with: @project.purpose
+
+    page.attach_file("Choose File", 'spec/fixtures/images/Dillon.jpg')
+    click_button 'Create Project'
+    expect(page).to have_content 'Portfolio picture'
   end
 end
