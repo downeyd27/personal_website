6575afdbb92f2a7e85399faf3e724b7c9555a61d
Afton Downey <downeydillon@gmail.com>
2016-05-10 09:13:25 -0700

    Remove unused validations.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,21 +1,12 @@
 class  Project < ActiveRecord::Base
-  validates_presence_of :name, :description, :purpose, :technology, :url, :image
-
+  validates_presence_of :name, :description, :purpose, :technology
+  
   validates :name, length: { maximum: 50 },
                    uniqueness: true
 
-  validates :url, length: { maximum: 50 },
-                  uniqueness: true,
-                  format: {
-                    with: /http?:\/\/[\S]+/,
-                    message: "only http urls allowed."
-                  }
-
   validates :description, length: { maximum: 140 }
-
-  validates :purpose, length: { maximum: 140 }
-
-  validates :technology, length: { maximum: 140 }
+  validates :purpose,     length: { maximum: 140 }
+  validates :technology,  length: { maximum: 140 }
 
   # This method associates the attribute ":avatar" with a file attachment
   has_attached_file :avatar,
