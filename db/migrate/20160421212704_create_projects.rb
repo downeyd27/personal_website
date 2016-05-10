bf18e3a643808469d5278ebd045b12ea2274f8f5
Afton Downey <downeydillon@gmail.com>
2016-05-10 09:14:28 -0700

    Remove unused fields.

--- db/migrate/20160421212704_create_projects.rb
+++ db/migrate/20160421212704_create_projects.rb
@@ -2,7 +2,6 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :url
 
       t.timestamps null: false
     end
--- db/migrate/20160506004356_add_columns_to_project.rb
+++ db/migrate/20160506004356_add_columns_to_project.rb
@@ -2,6 +2,5 @@ class AddColumnsToProject < ActiveRecord::Migration
   def change
     add_column :projects, :technology, :string
     add_column :projects, :purpose, :string
-    add_column :projects, :image, :string
   end
 end
