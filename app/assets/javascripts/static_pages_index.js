a0ee265a187bf6f2f25c82e0fbb97f90919d3e8d
Afton Downey <downeydillon@gmail.com>
2016-05-14 01:33:53 -0700

    Remove resume iframe jQuery.  Iframe doesn't bubble up and remove easily with jQuery.

--- app/assets/javascripts/static_pages_index.js
+++ app/assets/javascripts/static_pages_index.js
@@ -1,18 +1,3 @@
 // $(document).ready(function(){
-//   // On click of resume link
-//   $(".navLinkContainer__resumeLink").on ("click", function(){
-//     var iframe = document.createElement('iframe');
-//     iframe.setAttribute("src", "https://resume.creddle.io/embed/1rktq4ubpei");
-//     iframe.width = 850 + "px";
-//     iframe.height = 1100 + "px";
-//
-//     $("main").prepend("<div id='iframeContainer'></div>");
-//     $("#iframeContainer").html(iframe).append("<div id='close'>Close</div>");
-//   }); // End of on click of resume link
-//
-//   //On click of resume Close tab
-//   $("#close").on("click", function(){
-//     $("#iframeContainer").remove();
-//   }); // End of on click of resume Close tab
 //
 // });
