0a0cd1f748605fc7bb676bb62224de1f7ed5153b
Afton Downey <downeydillon@gmail.com>
2016-05-16 11:12:30 -0700

    Refactor s3 credentials.

--- app/models/project.rb
+++ app/models/project.rb
@@ -19,11 +19,15 @@ class  Project < ActiveRecord::Base
   },
   default_url: "/images/:style/missing.png",
   storage: :s3,
-  bucket: "downeyd27",
-  s3_host_name: 's3-us-west-2.amazonaws.com'
+  s3_host_alias: "https://#{Rails.application.secrets.aws_region}.amazonaws.com/#{Rails.application.secrets.s3_bucket_name}"
+  s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  def s3_credentials
+    { bucket: "downeyd27", access_key_id: Rails.application.secrets.aws_access_key_id, secret_access_key: Rails.application.secrets.aws_secret_access_key }
+  end
 
   def self.cached_find(id)
     Rails.cache.fetch(['project, id'], expires_in: 1.day) { find(id) }
