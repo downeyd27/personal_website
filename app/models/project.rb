e50c3a568f409abefd505b0510633e125df9ae6c
Afton Downey <downeydillon@gmail.com>
2016-05-16 11:48:00 -0700

    Add s3_host_alias to proc.

--- app/models/project.rb
+++ app/models/project.rb
@@ -19,14 +19,17 @@ class  Project < ActiveRecord::Base
   },
   default_url: "/images/:style/missing.png",
   storage: :s3,
-  s3_host_alias: "https://#{Rails.application.secrets.aws_region}.amazonaws.com/#{Rails.application.secrets.s3_bucket_name}",
   s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
 
   # Validate the attached image is image/jpg, image/png, etc
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def s3_credentials
-    { bucket: "downeyd27", access_key_id: Rails.application.secrets.aws_access_key_id, secret_access_key: Rails.application.secrets.aws_secret_access_key }
+    { 
+      bucket: "downeyd27",
+      access_key_id: Rails.application.secrets.aws_access_key_id, secret_access_key: Rails.application.secrets.aws_secret_access_key
+      s3_host_alias: "https://#{Rails.application.secrets.aws_region}.amazonaws.com/#{Rails.application.secrets.s3_bucket_name}",
+    }
   end
 
   def self.cached_find(id)
