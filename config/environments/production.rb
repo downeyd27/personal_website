270df028f0947dfa4c77e20768725265db03aa04
Afton Downey <downeydillon@gmail.com>
2016-05-16 11:28:37 -0700

    Change s3_host_name to s3_host_alias.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_host_alias: "https://#{Rails.application.secrets.aws_region}.amazonaws.com/#{Rails.application.secrets.s3_bucket_name}",
+    # s3_host_name: 's3-us-west-2.amazonaws.com',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
