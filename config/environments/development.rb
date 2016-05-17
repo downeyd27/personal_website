1ed417a5bbdf2ca8a6741ab39c0c009427d095b4
Afton Downey <downeydillon@gmail.com>
2016-05-17 16:20:05 -0700

    Remove comment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,6 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  # config.cache_store = :dalli_store, 'Localhost', { namespace: 'dev-dillondowney.herokuapp.com' }
   config.cache_store = :dalli_store,
                     (ENV["MEMCACHIER_SERVERS"] || "").split(","),
                     { :username => ENV["MEMCACHIER_USERNAME"],
