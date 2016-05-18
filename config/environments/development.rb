2d10896c5a1dab15cff69a3213b01563ddb9638a
Afton Downey <downeydillon@gmail.com>
2016-05-18 10:01:53 -0700

    Remove caching features and some page image paths to local directory.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,23 +29,23 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store,
-                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                    { :username => ENV["MEMCACHIER_USERNAME"],
-                      :password => ENV["MEMCACHIER_PASSWORD"],
-                      :failover => true,
-                      :socket_timeout => 1.5,
-                      :socket_failure_delay => 0.2,
-                      :pool_size => 5
-                    }
+  config.action_controller.perform_caching = false
+  # config.cache_store = :dalli_store,
+  #                   (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  #                   { :username => ENV["MEMCACHIER_USERNAME"],
+  #                     :password => ENV["MEMCACHIER_PASSWORD"],
+  #                     :failover => true,
+  #                     :socket_timeout => 1.5,
+  #                     :socket_failure_delay => 0.2,
+  #                     :pool_size => 5
+  #                   }
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,11 @@ Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #########################################
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  # Changed to false 05/18/16 9:23am
+  config.cache_classes = false
+  #########################################
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
