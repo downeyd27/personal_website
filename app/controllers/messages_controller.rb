358a6a135ffc2dff396a9a0ad6e1b3face2c334f
Afton Downey <downeydillon@gmail.com>
2016-05-23 15:31:50 -0700

    Working twilio from local host.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,40 +5,6 @@ class MessagesController < ApplicationController
   # If using Devise gem for authorization
   #skip_before_filter :authenticate_user!, :only => "reply"
 
-  rescue_from StandardError do |exception|
-    trigger_sms_alerts(exception)
-  end
-
-  def trigger_sms_alerts(e)
-    @alert_message = "
-    [This is a test] ALERT!
-    It appears the server is having issues.
-    Exception: #{e}.
-    Go to: http://newrelic.com for more details."
-    @image_url = "http://howtodocs.s3.amazonaws.com/new-relic-monitor.png"
-
-    @admin_list = YAML.load_file('config/administrators.yml')
-
-    begin
-      @admin_list.each do |admin|
-        phone_number = admin['phone_number']
-        send_message(phone_number, @alert_message, @image_url)
-      end
-
-      flash[:success] = "Exception: #{e}. Administrators will be notified."
-    rescue
-      flash[:alert] = "Something went wrong."
-    end
-    redirect_to '/'
-  end
-
-  def index
-  end
-
-  def server_error
-    raise 'A test exception'
-  end
-
   def new
   end
 
@@ -62,24 +28,8 @@ class MessagesController < ApplicationController
   private
 
     def boot_twilio
-      account_sid = Rails.application.secrets.twilio_test_sid
-      auth_token  = Rails.application.secrets.twilio_test_token
+      account_sid = Rails.application.secrets.twilio_sid
+      auth_token  = Rails.application.secrets.twilio_token
       @client = Twilio::REST::Client.new account_sid, auth_token
-    end
-
-    def send_message(phone_number, alert_message, image_url)
-
-      @twilio_number = ENV['TWILIO_NUMBER']
-      
-      boot_twilio
-
-      message = @client.account.messages.create(
-      :from => @twilio_number,
-      :to => phone_number,
-      :body => alert_message,
-      # US phone numbers can make use of an image as well.
-      # :media_url => image_url
-      )
-      puts message.to
     end
 end
