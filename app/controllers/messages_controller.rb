6d5050d27c1fa88581c2c5abf124b1f2940ce320
Afton Downey <downeydillon@gmail.com>
2016-05-23 15:56:51 -0700

    Change number and refactor.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,24 +6,20 @@ class MessagesController < ApplicationController
   #skip_before_filter :authenticate_user!, :only => "reply"
 
   def new
-    account_sid = Rails.application.secrets.twilio_sid
-    auth_token  = Rails.application.secrets.twilio_token
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    boot_twilio
   end
 
   def reply
     body = params[:body]
     from_phone_number = params[:phone]
-    account_sid = Rails.application.secrets.twilio_sid
-    auth_token  = Rails.application.secrets.twilio_token
-    @client = Twilio::REST::Client.new account_sid, auth_token
 
+    boot_twilio
     @twilio_number = Rails.application.secrets.twilio_number
 
     @client.account.messages.create({
       from: @twilio_number,
       # from: '+15005550006', # Twilio test number
-      to:   '19252128979',
+      to:   '18056805036',
       body: "#{body}",
     })
     redirect_to root_path
